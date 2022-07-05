67e6e536042b06f185b1fda2d3ed960e2de35665
HAGSAL <natthajak01@gmail.com>
2022-07-05 14:20:50 +0700

    first commit

---
+++ work-1.py
@@ -0,0 +1,12 @@
+print("hello M.6/14")
+print("My nanme is Natthajak Sisensui")
+print("My nick name is Aomsin")
+print("I am 17 years old")
+print("I study at Chonrasadornumrung")
+print("I live in Chonburi")
+print("My favorite color is Black")
+print("My favorite food is Pad kra pao")
+print("My favorite subject is Math")
+print("My birthday is 17th March 2005")
+print("I hate Science")
+# ชื่อ ณัฐจักขณ์ สีเสนซุย เลขที่25
---
+++ work-2.py
@@ -0,0 +1,24 @@
+name="Natthajak"
+lastname="Sisensui"
+nickname="Aomsin"
+age="17"
+educate="Chonrasadornumrung"
+country="Chonburi"
+favcolor="Black"
+favfood="Pad kra pao"
+favsubject="Math"
+birthday="17/03/2005"
+hate="Science"
+
+print(name)
+print(lastname)
+print(nickname)
+print(age)
+print(educate)
+print(country)
+print(favcolor)
+print(favfood)
+print(favsubject)
+print(birthday)
+print(hate)
+# ชื่อ ณัฐจักขณ์ สีเสนซุย เลขที่25
