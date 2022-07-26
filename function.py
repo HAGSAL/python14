a8495e949ecb7de021f9a3a4c61d196f86575978
HAGSAL <natthajak01@gmail.com>
2022-07-26 14:11:23 +0700

    commit2

---
+++ function.py
@@ -0,0 +1,12 @@
+#สร้างFunction
+def hello():
+    print("hello,world!")
+    print("Natthajak")
+hello()
+#เรียกใช้งานฟังก์ชัน
+hello()
+#ส่งค่าผ่านparameter+return
+def sum(x,y):
+    return x + y
+ans = sum(8,6)/2
+print(ans)
